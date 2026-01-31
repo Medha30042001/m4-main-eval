c327ae01ef8a7f58e8bcbfb28f4b014d81cb5e1e
Medha Adepu <medha.adepucse@gmail.com>
2026-01-31 21:09:26 +0530

    push two

--- src/server.js
+++ src/server.js
@@ -3,14 +3,17 @@ import 'dotenv/config'
 import userRoutes from './routes/user.routes.js'
 import vehicleRoutes from './routes/vehicle.routes.js'
 import tripRoutes from './routes/trip.routes.js'
+import { notFound } from './middlewares/notFound.js'
 
 const app = express();
 
 app.use(express.json());
 app.use('/users', userRoutes);
+
 app.use('/vehicles', vehicleRoutes);
 app.use('/trips', tripRoutes);
 
+app.use(notFound);
 app.listen(process.env.PORT, () => {
     console.log(`Server running on http://loclhost:${process.env.PORT}`);
 })
